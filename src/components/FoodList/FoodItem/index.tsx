0c60ec827d5aad66fcf65860c3f8304542955d13
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2024-02-04 05:30:30 +0900

    feat: 썸네일 lazy loading 기능 구현

--- src/components/FoodList/FoodItem/index.tsx
+++ src/components/FoodList/FoodItem/index.tsx
@@ -1,10 +1,38 @@
+import { useLayoutEffect, useRef, useState } from 'react'
 import { Food } from '../../../types'
 import Wrapper from './style'
 
 const FoodItem = ({ food }: { food: Food }) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+
+  const observer = useRef<IntersectionObserver>()
+  const imgRef = useRef<HTMLImageElement>(null)
+
+  useLayoutEffect(() => {
+    observer.current = new IntersectionObserver(intersectionObserver)
+    imgRef.current && observer.current.observe(imgRef.current)
+  }, [])
+
+  const intersectionObserver = (
+    entries: IntersectionObserverEntry[],
+    io: IntersectionObserver,
+  ) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        io.unobserve(entry.target)
+        setIsVisible(true)
+      }
+    })
+  }
   return (
     <Wrapper className="food-item">
-      <img className="food-image" src={food.strMealThumb} alt={food.strMeal} />
+      <img
+        className="food-image"
+        loading="lazy"
+        src={isVisible ? food.strMealThumb : undefined}
+        ref={imgRef}
+        alt={food.strMeal}
+      />
       <p className="food-name">{food.strMeal}</p>
     </Wrapper>
   )
